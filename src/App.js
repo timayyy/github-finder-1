d7f461abe21afdae5765a0579260b389a6e9d09c
timiistimi <timilehin65@gmail.com>
2020-05-20 18:04:17 -0600

    created single user page

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Users from "./components/users/Users";
 import Search from "./components/users/Search";
 import Alert from "./components/layouts/Alert";
 import About from "./components/pages/About";
+import User from "./components/users/User";
 import axios from "axios";
 import "./App.css";
 
 class App extends Component {
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null,
   };
@@ -27,6 +29,16 @@ class App extends Component {
     this.setState({ users: res.data.items, loading: false });
   };
 
+  //Get a single Github user
+  getUser = async (username) => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ user: res.data, loading: false });
+  };
+
   //Clear users from state
   clearUsers = () => this.setState({ users: [], loading: false });
 
@@ -38,7 +50,7 @@ class App extends Component {
   };
 
   render() {
-    const { users, loading } = this.state;
+    const { users, user, loading } = this.state;
 
     return (
       <Router>
@@ -63,6 +75,18 @@ class App extends Component {
                 )}
               />
               <Route exact path='/about' component={About} />
+              <Route
+                exact
+                path='/user/:login'
+                render={(props) => (
+                  <User
+                    {...props}
+                    getUser={this.getUser}
+                    user={user}
+                    loading={loading}
+                  />
+                )}
+              />
             </Switch>
           </div>
         </div>
