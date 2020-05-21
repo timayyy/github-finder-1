dcf80850b24e197e2d2d5b23ef825d2931678ed4
timiistimi <timilehin65@gmail.com>
2020-05-21 17:55:11 -0600

    Added repo functionality

--- src/components/repos/Repos.js
+++ src/components/repos/Repos.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import React from "react";
+import PropTypes from "prop-types";
+import RepoItem from "./RepoItem";
 
-const Repos = ({repos}) => {
-    return Repos.map(repo=> <RepoItem repo={repo} key={repo.id}) />
-}
-export default Repos
+const Repos = ({ repos }) => {
+  return repos.map((repo) => <RepoItem repo={repo} key={repo.id} />);
+};
+
+Repos.propTypes = {
+  repos: PropTypes.array.isRequired,
+};
+export default Repos;
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from "react";
 import Spinner from "../layouts/Spinner";
+import Repos from "../repos/Repos";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
@@ -12,6 +13,7 @@ export class User extends Component {
   static propTypes = {
     loading: PropTypes.bool,
     user: PropTypes.object.isRequired,
+    repos: PropTypes.array.isRequired,
     getUser: PropTypes.func.isRequired,
     getUserRepos: PropTypes.func.isRequired,
   };
@@ -33,7 +35,7 @@ export class User extends Component {
       hireable,
     } = this.props.user;
 
-    const { loading } = this.props;
+    const { loading, repos } = this.props;
 
     if (loading) return <Spinner />;
     return (
@@ -100,6 +102,7 @@ export class User extends Component {
           <div className='badge badge-light'>Public Repos: {public_repos}</div>
           <div className='badge badge-dark'>Public Gists: {public_gists}</div>
         </div>
+        <Repos repos={repos} />
       </Fragment>
     );
   }
