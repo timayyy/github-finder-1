5da8f15304136c446288e9438e709c44c4b722e8
timiistimi <timilehin65@gmail.com>
2020-05-23 18:06:15 -0600

    Added context and reducer

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -6,7 +6,7 @@ import {
   SEARCH_USERS,
   SET_LOADING,
   CLEAR_USERS,
-  GET_USERS,
+  GET_USER,
   GET_REPOS,
 } from "../types";
 
@@ -19,15 +19,43 @@ const GithubState = (props) => {
   };
   const [state, dispatch] = useReducer(GithubReducer, initialState);
 
-  //search user
+  //search users
+  const searchUsers = async (text) => {
+    setLoading();
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    dispatch({
+      type: SEARCH_USERS,
+      payload: res.data.items,
+    });
+  };
 
   //get user
+  const getUser = async (username) => {
+    setLoading();
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    dispatch({ type: GET_USER, payload: res.data });
+  };
 
   //get repos
+  const getUserRepos = async (username) => {
+    setLoading();
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    dispatch({ type: GET_REPOS, payload: res.data });
+  };
 
   //clear users
+  const clearUsers = () => dispatch({ type: CLEAR_USERS });
 
   //set loading
+  const setLoading = () => dispatch({ type: SET_LOADING });
 
   return (
     <GithubContext.Provider
@@ -36,6 +64,10 @@ const GithubState = (props) => {
         user: state.user,
         repos: state.repos,
         loading: state.loading,
+        searchUsers,
+        clearUsers,
+        getUser,
+        getUserRepos,
       }}
     >
       {props.children}
